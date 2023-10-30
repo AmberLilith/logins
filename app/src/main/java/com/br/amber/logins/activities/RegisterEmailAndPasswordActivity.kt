c9eca3ad181b98d8bf05112e7727a9f4cdef50b9
AmberLilith <amber.lilith.ordone@gmail.com>
2023-10-30 01:16:38 -0300

    Incluída antenticação com email e senha

--- app/src/main/java/com/br/amber/logins/activities/RegisterEmailAndPasswordActivity.kt
+++ app/src/main/java/com/br/amber/logins/activities/RegisterEmailAndPasswordActivity.kt
@@ -1,4 +1,4 @@
-package com.br.amber.logins
+package com.br.amber.logins.activities
 
 import android.content.ContentValues.TAG
 import android.content.Intent
@@ -9,39 +9,38 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.br.amber.logins.activities.ListLoginsActivity
-import com.br.amber.logins.activities.RegisterEmailAndPasswordActivity
+import com.br.amber.logins.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class RegisterEmailAndPasswordActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var textViewEmail: TextView
-    private lateinit var  textViewPassword: TextView
+    private lateinit var editTextEmail: EditText
+    private lateinit var  editTextPassword: EditText
+    private lateinit var buttonRegister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val buttonAuth = findViewById<Button>(R.id.buttonAuth)
-        val editTextEmail = findViewById<EditText>(R.id.editTextAuthEmail)
-        val editTextPassword = findViewById<EditText>(R.id.editTextAuthPassword)
+        setContentView(R.layout.activity_register_email_and_password)
+
+        editTextEmail = findViewById(R.id.editTextRegisterEmail)
+         editTextPassword = findViewById(R.id.editTextRegisterPassword)
+        buttonRegister =  findViewById(R.id.buttonRegister)
         auth = Firebase.auth
 
-
-
-        buttonAuth.setOnClickListener {
-            auth.signInWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
+        buttonRegister.setOnClickListener{
+            auth.createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
+                        Log.d(TAG, "createUserWithEmail:success")
                         val user = auth.currentUser
                         updateUI(user)
                     } else {
                         // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Toast.makeText(
                             baseContext,
                             "Authentication failed.",
@@ -51,31 +50,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
         }
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload()
-        }else{
-            val intent = Intent(this, RegisterEmailAndPasswordActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun reload() {
-        val user = auth.currentUser
-        user?.reload()
-            ?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // User data has been reloaded successfully
-                } else {
-                    // Handle the error
-                    // task.exception can provide more details about the error
-                }
-            }
     }
 
     private fun updateUI(user: FirebaseUser?) {
@@ -107,5 +81,27 @@ class MainActivity : AppCompatActivity() {
             // editTextEmail.text.clear()
             // editTextPassword.text.clear()
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun reload() {
+        val user = auth.currentUser
+        user?.reload()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // User data has been reloaded successfully
+                } else {
+                    // Handle the error
+                    // task.exception can provide more details about the error
+                }
+            }
     }
 }
