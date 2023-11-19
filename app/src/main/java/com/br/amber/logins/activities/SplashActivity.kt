68945bebb28e1152fad5e9ebe58f8f9bafd345a2
AmberLilith <amber.lilith.ordone@gmail.com>
2023-11-19 16:03:10 -0300

    Adicionada tela splash

---
+++ app/src/main/java/com/br/amber/logins/activities/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.br.amber.logins.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.br.amber.logins.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val tempoEspera = 6000L
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, AuthenticationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, tempoEspera)
+    }
+}
